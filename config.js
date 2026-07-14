9e1a414fa2ca6392e26caf217652e654d60603a7
wys1110 <48542153+wys1110@users.noreply.github.com>
2026-07-14 19:55:03 +0900

    Connect Supabase project

--- config.js
+++ config.js
@@ -1,1 +1,4 @@
-window.FAMILY_CONFIG = window.FAMILY_CONFIG || { supabaseUrl: "", supabaseAnonKey: "" };
+window.FAMILY_CONFIG = {
+  supabaseUrl: "https://ljutcgmgtqfkwkxdbiyb.supabase.co",
+  supabaseAnonKey: "sb_publishable_PlEeuwC__Ft2Gtv3iT4OcA_hV461WbY",
+};
